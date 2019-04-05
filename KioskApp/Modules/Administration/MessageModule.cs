403d8c6a4bace9bef827d99a03bae947ea2c5214
Malachi Austin <malachiaustin02@outlook.com>
2019-04-05 17:34:08 -0400

    [BOT] Started Message-utility module; Starting EventListeners.

---
+++ KioskApp/Modules/Administration/MessageModule.cs
@@ -0,0 +1,39 @@
+﻿#region USING DIRECTIVES
+
+using KioskApp.Common;
+using KioskApp.Common.Attributes;
+using KioskApp.Database;
+using KioskApp.Exceptions;
+using KioskApp.Extensions;
+
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+using System;
+using System.Threading.Tasks;
+
+#endregion USING DIRECTIVES
+
+namespace KioskApp.Modules.Administration
+{
+    [Group("message"), Module(ModuleType.Administration), NotBlocked]
+    [Aliases("m", "msg")]
+    [RequireOwnerOrPermissions(Permissions.ManageMessages)]
+    [Description("Utility commands for text-channels.")]
+    public class MessageModule : AppModule
+    {
+        public MessageModule(SharedData shared, DatabaseContextBuilder dbb) 
+            : base(shared, dbb)
+        {
+            this.ModuleColor = DiscordColor.Green;
+        }
+
+        [Command("pin")]
+        public async Task PinMessageAsync(CommandContext ctx )
+        {
+
+        }
+    }
+}
